24ffc5995ba1b619eae98e8d65ef1d75cc858ad2
AIT ALI YAHIA Rayane <aitaliyahia.rayane.pro@gmail.com>
2024-11-01 22:31:04 +0000

    add dangling pointer + ownership move

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+/* 
+fn testing_daggling_pointer() -> &String {
+    let mut s = String::from("hello");
+    &s
+}
+    */
+
+fn main() {
+    println!("Hello, world!");
+    /*  s1 took ownership of s, and s goes out of the scope
+    let s = String::from("hello");
+    let s1 = s;
+    println!("{}", s);
+    */
+
+    // let mut string = testing_daggling_pointer();
+
+}
